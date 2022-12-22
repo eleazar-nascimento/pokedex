bb930c140b27915c2b26acf5729958e97a5f6b90
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2022-12-22 11:38:46 -0300

    refactor: removes the external import of the theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,36 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import { theme } from './styles/theme';
+import { ChakraProvider, ColorModeScript, extendTheme, ThemeConfig } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
+  const config: ThemeConfig = {
+    initialColorMode: 'light',
+    useSystemColorMode: true,
+  }
+
+  const theme = extendTheme({
+    config,
+
+    styles: {
+      global: {
+        body: {
+          padding: 0,
+          margin: 0,
+          color: '#222',
+          background: 'orangered',
+        },
+        h2: {
+          fontSize: '44px',
+          fontWeight: 'bolder',
+        },
+        a: {
+          color: 'inherit',
+          textDecoration: 'none'
+        },
+
+      }
+    }
+  })
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
